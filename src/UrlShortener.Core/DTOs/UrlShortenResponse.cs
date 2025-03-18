cf3f77f1bb6d2e819ac4f323a56a92858261a914
Kien <kientngcd220284@fpt.edu.>
2025-03-18 16:02:52 +0700

    Xóa toàn bộ file cũ

--- src/UrlShortener.Core/DTOs/UrlShortenResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace UrlShortener.Core.DTOs
-{
-    public class UrlShortenResponse
-    {
-        public string OriginalUrl { get; set; } = string.Empty;
-        public string ShortUrl { get; set; } = string.Empty;
-        public string Code { get; set; } = string.Empty;
-    }
-}
--- src/UrlShortener.Infrastructure/Class1.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace UrlShortener.Infrastructure;
-public class Class1
-{
-
-}
